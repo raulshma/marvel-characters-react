ae219556a115fbdc988cf488a73f63c6a77f2fba
raulshma <raulshma@gmail.com>
2020-09-05 22:32:35 +0000

    Deploying to gh-pages from  @ c9863fe575dbafe1032b4d54acd506fbe77d3066 🚀

---
+++ precache-manifest.0528143a79642c1dc2055dbc1880b7b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67871dee7c2071c8ccbbb1443f52fcf1",
+    "url": "/marvel-characters-react/index.html"
+  },
+  {
+    "revision": "b1985352d4aef7d2b0f3",
+    "url": "/marvel-characters-react/static/css/main.71b29560.chunk.css"
+  },
+  {
+    "revision": "da704f1f30988e2c29ee",
+    "url": "/marvel-characters-react/static/js/2.09b75e28.chunk.js"
+  },
+  {
+    "revision": "6cc9b2cb4db1447b734d925a70c8c404",
+    "url": "/marvel-characters-react/static/js/2.09b75e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1985352d4aef7d2b0f3",
+    "url": "/marvel-characters-react/static/js/main.d74d1668.chunk.js"
+  },
+  {
+    "revision": "870f19897bf2b7ba4e73",
+    "url": "/marvel-characters-react/static/js/runtime-main.6a76a8af.js"
+  }
+]);
